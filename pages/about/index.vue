0721ec934a145a913cad4ae208e95f175f9a2f10
Faeq <faeqfaisal@hotmail.co.uk>
2025-08-12 17:25:42 +0100

    :construction: About me page layout

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,9 +1,108 @@
+<script lang="ts" setup>
+import { ref, useTemplateRef } from 'vue';
+import type { StepperItem } from '@nuxt/ui'
+
+const items: StepperItem[] = [
+  {
+    title: 'Introduction',
+    description: 'A little on me',
+    icon: 'i-lucide-house'
+  },
+  {
+    title: '2010',
+    description: '6 years old',
+    icon: 'i-lucide-truck'
+  },
+  {
+    title: '2016',
+    description: '12 years old',
+    icon: 'i-lucide-truck'
+  },
+  {
+    title: '2018',
+    description: '14 years old',
+    icon: 'i-lucide-truck'
+  },
+  {
+    title: '2020',
+    description: '16 years old',
+    icon: 'i-lucide-truck'
+  },
+  {
+    title: '2022',
+    description: '18 years old',
+    icon: 'i-lucide-truck'
+  },
+  {
+    title: '2025',
+    description: '21 years old - now',
+    icon: 'i-lucide-truck'
+  },
+]
+
+const stepper = useTemplateRef('stepper')
+const active = ref(0)
+
+</script>
+
 <template>
   <div class="p-8 h-full w-full">
-    <div class="flex flex-col justify-center pl-8 h-full w-full">
-      <div class="font-bold mb-5 varela" style="line-height: 1;">
-        <p class="text-[4rem]">About me</p>
-        <p class="text-[2.3rem]">subtitle</p>
+    <div class="flex h-full w-full">
+      <UCard
+        class="m-4 h-full w-96 opacity-80 cardShadow border border-[var(--ui-border)]">
+        <template #header>
+          <div class="font-bold varela" style="line-height: 1;">
+            <p class="text-[3rem]">About me</p>
+            <p class="text-[1rem]">Who I am and what I get up to</p>
+          </div>
+        </template>
+
+        <div class="min-h-[60vh]">
+          <UStepper ref="stepper" :items="items" orientation="vertical"
+            v-model="active" class="h-full w-full -mt-4 stepper"
+            style="--ui-primary: #4a5565"
+            :ui="{ separator: 'h-8 transition-all ease-in-out duration-200', item: 'mt-4' }"
+            disabled />
+        </div>
+
+        <template #footer>
+          <div>
+
+            <div class="flex gap-2 justify-between mt-4">
+              <UButton leading-icon="i-lucide-arrow-left"
+                :disabled="!stepper?.hasPrev" @click="stepper?.prev()">
+                Prev
+              </UButton>
+
+              <UButton trailing-icon="i-lucide-arrow-right"
+                :disabled="!stepper?.hasNext" @click="stepper?.next()">
+                Next
+              </UButton>
+            </div>
+          </div>
+        </template>
+      </UCard>
+      <!--Snap scroll these-->
+      <div
+        class="flex flex-col h-full w-full mr-8 mt-4 overflow-y-scroll overflow-x-hidden"
+        data-lenis-prevent>
+        <UCard
+          class="m-4 h-60 opacity-80 cardShadow border border-[var(--ui-border)]"
+          v-for="i in Array(8).keys()">
+          <template #header>
+            header
+          </template>
+
+          <div class="min-h-20">
+            content
+          </div>
+
+          <template #footer>
+            <div>
+              footer
+            </div>
+          </template>
+        </UCard>
       </div>
     </div>
   </div>
